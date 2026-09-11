a46bccf02353d105524f8a5aaaa671ac71e8d889
kameusagiyahoo <nywhatisbudou@yahoo.co.jp>
2026-09-11 09:11:49 +0900

    chore: set app version to 8.40.0

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import {createAppState} from './app/state.js';
 import {createAppRuntime} from './app/runtime.js';
 
-const APP_VERSION='8.39.0';
+const APP_VERSION='8.40.0';
 
 const runtime=createAppRuntime({
   state:createAppState(),
